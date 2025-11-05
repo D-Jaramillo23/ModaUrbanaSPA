7b8144f9b10b16632d8a2b0aefa3e968c59d7d91
D-Jaramillo23 <d.jaramillo@duocuc.cl>
2025-11-05 20:38:16 -0300

    Creo los const val correspondientes para "Login, Register, Home y Profile", Para dejarlos como valores constates y evitar equivocarme.

---
+++ app/src/main/java/com/modaurbana/spa/ui/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.modaurbana.spa.ui.navigation
+
+object Routes {
+    const val Login = "Login"
+    const val Register = "register"
+    const val Home = "Home"
+    const val Profile = "profile"
+}
